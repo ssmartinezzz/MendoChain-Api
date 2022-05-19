8d5f3c15650e4ce1c00534661db653910791014f
s.martinez <santiskatersan@gmail.com>
2022-05-19 18:46:17 -0300

    Views as Classes

--- api/backend/views.py
+++ api/backend/views.py
@@ -11,72 +11,57 @@ def hello_world(request):
     name = "Anonymous" if request.user.is_anonymous else request.user.first_name
     return Response({"hello": f"Welcome to DRF, {name}!"}, 200)
 
-
-class WineRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Wine.objects.all()
-    serializer_class = WineSerializer
-
-
 class WineList(generics.ListCreateAPIView):
-    queryset = Wine.objects.all()
+    queryset = Wine.objects.filter(visibility=1)
     serializer_class = WineSerializer
-
-
-class TransactionRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-
 
 class TransactionList(generics.ListCreateAPIView):
     queryset = Transaction.objects.filter(visibility=1)
     serializer_class = TransactionSerializer
 
-@api_view(['GET', 'POST'])
-def wine_api_view(request):
-    if request.method == 'GET':
-        wines = Wine.objects.filter(visibility=1)
-        wine_serializer = WineSerializer(wines, many=True)
-        return Response(wine_serializer.data, status = status.HTTP_200_OK)
 
-    elif request.method == 'POST':
-        wine_serializer = WineSerializer (data = request.data)
+class WineApiView(APIView):
+
+    def get(self, request, pk):
+        wine = Wine.objects.filter(id=pk).first()
+        wine_serializer = WineSerializer(wine)
+        return Response(wine_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        wine_serializer = WineSerializer(data=request.data)
         if wine_serializer.is_valid():
             wine_serializer.save()
-            return Response(wine_serializer.data, status = status.HTTP_201_CREATED)
-        return Response(wine_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(wine_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(wine_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT'])
-def wine_detail_api(request, pk=None):
-    wine = Wine.objects.filter(id=pk).first()
-    if wine:
-        if request.method == 'GET':
-            wine_serializer = WineSerializer(wine)
-            return Response(wine_serializer.data, status=status.HTTP_200_OK)
-        elif request.method == 'PUT':
+    def put(self, request, pk):
+        wine = Wine.objects.filter(id=pk).first()
+        if wine:
             wine_serializer = WineSerializer(wine, data=request.data)
             if wine_serializer.is_valid():
                 wine_serializer.save()
                 return Response(wine_serializer.data, status=status.HTTP_200_OK)
             return Response(wine_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response({'message':"Wine not found"}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'message': "Wine not found"}, status=status.HTTP_404_NOT_FOUND)
 
-@api_view(['POST', 'GET', 'PUT'])
-def create_block_transaction(request, pk=None):
-    transaction = Transaction.objects.filter(id=pk).first()
-    if request.method =='POST':
+class TransactionApiView(APIView):
+
+    def get(self, request, pk):
+        transaction = Transaction.objects.filter(id=pk).first()
+        if transaction:
+            transaction_serializer = TransactionSerializer(transaction)
+            return Response(transaction_serializer.data, status=status.HTTP_200_OK)
+        return Response({'message': "Transaction not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
         transaction_serializer = TransactionSerializer(data=request.data)
         if transaction_serializer.is_valid():
             transaction_serializer.save()
             return Response(transaction_serializer.data, status=status.HTTP_201_CREATED)
         return Response(transaction_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    if request.method == 'GET':
-        if transaction:
-            transaction_serializer = TransactionSerializer(transaction)
-            return Response(transaction_serializer.data, status = status.HTTP_200_OK)
-        return Response({'message': "Transaction not found"}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'PUT':
+    def put(self, request, pk):
+        transaction = Transaction.objects.filter(id=pk).first()
         if transaction:
             transaction_serializer = TransactionSerializer(transaction, data=request.data)
             if transaction_serializer.is_valid():
@@ -84,11 +69,3 @@ def create_block_transaction(request, pk=None):
                 return Response(transaction_serializer.data, status=status.HTTP_201_CREATED)
             return Response(transaction_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response({'message': "Transaction not found"}, status=status.HTTP_404_NOT_FOUND)
-
-
-
-
-
-
-
-
